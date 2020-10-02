975793c645270416342b6d6644a65b3889bb5fda
gavrihaddad <gavrihaddad@gmail.com>
2020-10-02 11:48:11 +0300

    Problem 1: Multiples of 3 and 5
    Also adds the Problems folder, and the UsefulFunctions class.

---
+++ Project-Euler/Problems/Problem1MultiplesOf3and5.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler.Problems
+{
+    static class Problem1MultiplesOf3and5
+    {
+        /// <summary>
+        /// Solves problem 1
+        /// </summary>
+        /// <returns> The sum of multiples of 3 or 5 bellow 1000 </returns>
+        public static int GetSum()
+        {
+            return GetMultiplesOf3and5().Sum();
+        }
+
+        private static List<int> GetMultiplesOf3and5()
+        {
+            List<int> Multiples = new List<int>();
+
+            for (int i = 3; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0) 
+                {
+                    Multiples.Add(i);
+                }
+
+            }
+
+            return Multiples;
+        }
+    }
+}
---
+++ Project-Euler/UsfulFunctions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler
+{
+    /// <summary>
+    /// This class contains functions that are usful for more than 1 problem.
+    /// </summary>
+    public static class UsfulFunctions
+    {
+
+    }
+}
