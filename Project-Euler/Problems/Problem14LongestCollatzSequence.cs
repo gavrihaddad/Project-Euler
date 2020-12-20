e2f9f5abf405d034e533f7dcaa8b32a67c3aef42
gavriel haddadGh3220010130 <gavrihaddad@gmail.com>
2020-12-20 14:37:08 +0200

    Test commit 2

--- Project-Euler/Problems/Problem14LongestCollatzSequence.cs
+++ Project-Euler/Problems/Problem14LongestCollatzSequence.cs
@@ -8,9 +8,6 @@ namespace Project_Euler.Problems
 {
     public static class Problem14LongestCollatzSequence
     {
-        private static readonly List<int> candidates = Enumerable.Range(2, 9).ToList();
-
-
         #region Brute force
 
         public static int GetNum1()
@@ -18,6 +15,12 @@ namespace Project_Euler.Problems
             int result = 0;
             int maxLength = 0;
 
+            // All the numbers from 1 to 500000 can be multiplied by 2
+            // a certain amount of times and end up in an even number in
+            // the 500001-1000000 range, therfore they cannot have the
+            // longest sequence (because the coresponding number is even,
+            // so the next step will be to divide it by 2, and so on until
+            // we get to the number we started from in the 1-500000 range).
             for (int i = 500001; i <= 1000000; i++)
             {
                 long num = i;
@@ -38,8 +41,69 @@ namespace Project_Euler.Problems
             }
 
             return result;
+        }
 
-            long GetNext(long num)
+        #endregion
+
+
+        public static int GetNum2()
+        {
+            Dictionary<long, int> lengths = new Dictionary<long, int>();
+
+            int result = 0;
+            int maxLength = 0;
+
+            // Look in GetNum1 for an explenation.
+            for (int i = 500001; i <= 1000000; i++)
+            {
+                long num = i;
+                int length = 0;
+                List<long> trail = new List<long>();
+
+                while (num != 1)
+                {
+                    if (lengths.ContainsKey(num))
+                    {
+                        length += lengths[num];
+
+                        for (int j = 0; j < trail.Count; j++)
+                        {
+                            lengths[trail[j]] += lengths[num];
+                        }
+
+                        break;
+                    }
+
+
+                    length++;
+
+                    trail.Add(num);
+                    for (int j = 0; j < trail.Count; j++)
+                    {
+                        if (lengths.ContainsKey(trail[j]))
+                        {
+                            lengths[trail[j]]++;
+                        }
+                        else
+                        {
+                            lengths.Add(trail[j], 1);
+                        }
+                    }
+
+                    num = GetNext(num);
+                }
+
+                if (length > maxLength) 
+                {
+                    maxLength = length;
+                    result = i;
+                }
+            }
+
+            return result;
+        }
+
+        private static long GetNext(long num)
             {
                 if (num % 2 == 0)
                 {
@@ -50,10 +114,5 @@ namespace Project_Euler.Problems
                     return num * 3 + 1;
                 }
             }
-        }
-
-        #endregion
-
-        //Test Commit
     }
 }
